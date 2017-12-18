a5b22badb9dc3202b67bf9dbd930197c2a13fe76
Cristina <crism.neagu@gmail.com>
2017-12-18 19:24:41 +0200

    lesson11

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -13,11 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.background.sync;
+import android.app.IntentService;
+import android.app.Notification;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 
-// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+import com.example.android.background.sync.ReminderTasks;
 
-//  TODO (10) Create a default constructor that calls super with the name of this class
+// COMPLETED (9) Create WaterReminderIntentService and extend it from IntentService
+public class WaterReminderIntentService extends IntentService {
 
-//  TODO (11) Override onHandleIntent
-//      TODO (12) Get the action from the Intent that started this Service
-//      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+    public WaterReminderIntentService() {
+        super("WaterReminderIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        String action = intent.getAction();
+        ReminderTasks.executeTask(this, action);
+    }
+}
+
+//  COMPLETED (10) Create a default constructor that calls super with the name of this class
+
+//  COMPLETED (11) Override onHandleIntent
+//      COMPLETED (12) Get the action from the Intent that started this Service
+//      COMPLETED (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
